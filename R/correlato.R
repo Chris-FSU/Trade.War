8b8ab954c1412f63b9826e0dc7d100dfab1a9487
Chris Main <cbm19e@my.fsu.edu>
2019-12-14 12:04:33 -0500

    Successfully save comp62
    Fix #1

--- R/correlato.R
+++ R/correlato.R
@@ -5,13 +5,15 @@ data.yr<-read_rds("data/by.year/1962.rds")
 
 # isolate exports
 by.exp<-pivot_wider(data.yr, names_from = exporter, values_from = value)
+by.exp[is.na(by.exp)] <- 0
 # store vector of columns
 expcols<-ncol(by.exp)-4
 
 # isolate imports
-by.imp<-pivot_wider(data.yr, names_from = importer, values_from = value)
+by.imp<-pivot_wider(data.yr, id_cols =c("exporter","sitc4") , names_from = importer, values_from = value)
+by.imp[is.na(by.imp)] <- 0
 # store vector of columns
-impcols<-ncol(by.imp)-4
+impcols<-ncol(by.imp)-2
 
 # Pearson's correlation statistic for exports from each pair of countries
 exp<-matrix(rep(NA,expcols*expcols),nrow=expcols)
@@ -29,7 +31,7 @@ for (i in 1:expcols) {
 for (i in 1:impcols) {
   for (j in 1:impcols) {
     ifelse(i==j,NA,
-           imp[i,j]<-cor(by.imp[i+4],by.imp[j+4],"pearson",use="pairwise.complete.obs"))
+           imp[i,j]<-cor(by.imp[i+2],by.imp[j+2],"pearson",use="pairwise.complete.obs"))
   }
 }
 
@@ -43,16 +45,19 @@ exp<-as.data.frame(exp) %>%
   rename(country.a = name, exp.comp = value)
 
 # Name import columns by country
-colnames(imp)<-names(by.imp)[5:149]
+colnames(imp)<-names(by.imp)[3:147]
 
 # Make imports a dyadic dataset with two columns for country a and country b
 imp<-as.data.frame(imp) %>%
-  mutate(country.b = names(by.imp)[5:149]) %>%
+  mutate(country.b = names(by.imp)[3:147]) %>%
   pivot_longer(cols = 1:145) %>%
   rename(country.a = name, imp.comp = value)
 
 # combine imports and exports
 comp.62<-left_join(exp,imp)
+
+# Save it
+write_rds(comp.62,"data/by.year/comp62.rds")
 
 # clean up mess
 rm(by.exp,by.imp,data.yr,exp,imp)
