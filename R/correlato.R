35beac410ea9a66865e0eb3f1c20a9fd45a3266f
Chris Main <cbm19e@my.fsu.edu>
2019-12-14 12:39:04 -0500

    Test run
    I wanted to make correlato.R do my work for me. I have succeeded.

--- R/correlato.R
+++ R/correlato.R
@@ -60,20 +60,36 @@ comp.62<-left_join(exp,imp)
 write_rds(comp.62,"data/by.year/comp62.rds")
 
 # clean up mess
-rm(by.exp,by.imp,data.yr,exp,imp)
+rm(list=ls())
 
 
 
+
+
+
+sets<- 1962:1999
+for (annum in 1:length(sets)){
+  
 ### Data for 1963
-data.yr<-filter(data, year == 1963)
+filename<-paste0("data/by.year/",sets[annum],".rds")
+data.yr<-read_rds(filename)
+
+# isolate exports
 by.exp<-pivot_wider(data.yr, names_from = exporter, values_from = value)
+by.exp[is.na(by.exp)] <- 0
+# store vector of columns
 expcols<-ncol(by.exp)-4
-by.imp<-pivot_wider(data.yr, names_from = importer, values_from = value)
-impcols<-ncol(by.imp)-4
+
+# isolate imports
+by.imp<-pivot_wider(data.yr, id_cols =c("exporter","sitc4") , names_from = importer, values_from = value)
+by.imp[is.na(by.imp)] <- 0
+# store vector of columns
+impcols<-ncol(by.imp)-2
 
 # Pearson's correlation statistic for exports from each pair of countries
 exp<-matrix(rep(NA,expcols*expcols),nrow=expcols)
 imp<-matrix(rep(NA,impcols*impcols),nrow=impcols)
+
 for (i in 1:expcols) {
   for (j in 1:expcols) {
     ifelse(i==j,NA,
@@ -83,7 +99,7 @@ for (i in 1:expcols) {
 for (i in 1:impcols) {
   for (j in 1:impcols) {
     ifelse(i==j,NA,
-           imp[i,j]<-cor(by.imp[i+4],by.imp[j+4],"pearson",use="pairwise.complete.obs"))
+           imp[i,j]<-cor(by.imp[i+2],by.imp[j+2],"pearson",use="pairwise.complete.obs"))
   }
 }
 
@@ -93,10 +109,17 @@ exp<-as.data.frame(exp) %>%
   mutate(country.b = names(by.exp)[5:(expcols+4)]) %>%
   pivot_longer(cols = 1:expcols) %>%
   rename(country.a = name, exp.comp = value)
-colnames(imp)<-names(by.imp)[5:(impcols+4)]
+colnames(imp)<-names(by.imp)[3:(impcols+2)]
 imp<-as.data.frame(imp) %>%
-  mutate(country.b = names(by.imp)[5:(impcols+4)]) %>%
+  mutate(country.b = names(by.imp)[3:(impcols+2)]) %>%
   pivot_longer(cols = 1:impcols) %>%
   rename(country.a = name, imp.comp = value)
-comp63<-left_join(exp,imp)
-rm(by.exp,by.imp,data.yr,exp,imp)
+
+# Combine them
+comp<-left_join(exp,imp)
+
+# Save it
+output.filename<-paste0("data/by.year/comp",sets[annum],".rds")
+write_rds(comp,output.filename)
+rm(comp,exp,imp,by.exp,by.imp,impcols,expcols)
+}
