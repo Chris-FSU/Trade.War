fd47d5614bb5028d1ca8faabc56a35168a8090b1
Chris Main <cbm19e@my.fsu.edu>
2019-12-14 09:39:10 -0500

    split the data
    I split the data int otiny little data dragons which can be wrangled one-by-one.
    
    For some reason, line 12 of correlato.R is not working properly. There are as many observations after the pivot_wider as before. This is my next challenge.

--- R/correlato.R
+++ R/correlato.R
@@ -4,20 +4,30 @@ data<-read_rds("data/massive.rds")
 
 ### Data for 1962
 data.yr<-filter(data, year == 1962)
+
+# isolate exports
 by.exp<-pivot_wider(data.yr, names_from = exporter, values_from = value)
+# store vector of columns
 expcols<-ncol(by.exp)-4
+
+# isolate imports
 by.imp<-pivot_wider(data.yr, names_from = importer, values_from = value)
+# store vector of columns
 impcols<-ncol(by.imp)-4
 
 # Pearson's correlation statistic for exports from each pair of countries
 exp<-matrix(rep(NA,expcols*expcols),nrow=expcols)
 imp<-matrix(rep(NA,impcols*impcols),nrow=impcols)
+
+# Place each Pearson's correlation of exports into the appropriate cell
 for (i in 1:expcols) {
   for (j in 1:expcols) {
     ifelse(i==j,NA,
            exp[i,j]<-cor(by.exp[i+4],by.exp[j+4],"pearson",use="pairwise.complete.obs"))
   }
 }
+
+# Place each Pearson's correlation  of imports into the appropriate cell
 for (i in 1:impcols) {
   for (j in 1:impcols) {
     ifelse(i==j,NA,
@@ -25,18 +35,28 @@ for (i in 1:impcols) {
   }
 }
 
-#
+# Name columns by country
 colnames(exp)<-names(by.exp)[5:149]
+
+# Make exports a dyadic dataset with two columns for country a and country b
 exp<-as.data.frame(exp) %>%
   mutate(country.b = names(by.exp)[5:149]) %>%
   pivot_longer(cols = 1:145) %>%
   rename(country.a = name, exp.comp = value)
+
+# Name import columns by country
 colnames(imp)<-names(by.imp)[5:149]
+
+# Make imports a dyadic dataset with two columns for country a and country b
 imp<-as.data.frame(imp) %>%
   mutate(country.b = names(by.imp)[5:149]) %>%
   pivot_longer(cols = 1:145) %>%
   rename(country.a = name, imp.comp = value)
+
+# combine imports and exports
 comp.62<-left_join(exp,imp)
+
+# clean up mess
 rm(by.exp,by.imp,data.yr,exp,imp)
 
 
